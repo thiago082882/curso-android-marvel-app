ced0d45b042683effd5be61960705581561053b3
thiago082882 <thiagocostadejesus2@gmail.com>
2024-01-18 23:05:50 -0300

    Configure nav base

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.ActivityMainBinding
 
@@ -17,11 +18,23 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
         navController = navHostFragment.navController
+        binding.bottomNavMain.setupWithNavController(navController)
         appBarConfiguration = AppBarConfiguration(
-            setOf()
+            setOf(R.id.characterFragment,R.id.favoritesFragment,R.id.aboutFragment)
         )
+
+        binding.toolbarApp.setupWithNavController(navController,appBarConfiguration)
+
+        navController.addOnDestinationChangedListener{ _,destination,_ ->
+            val isTopLevelDestination = appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if(!isTopLevelDestination){
+                binding.toolbarApp.setNavigationIcon(R.drawable.ic_back)
+            }
+
+        }
 
     }
 }
